f85aebeda9cfc373a5c7e8505700f3bdd1eeb05b
p15kara3 <p15kara3@ionio.gr>
2018-03-18 14:19:19 +0000

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -33,30 +33,27 @@ def scan(text,transition_table,accept_states):
 				return accept_states[state],pos
 
 			# current state is not accepting
-			return 'ERROR_TOKEN',pos
+			return 'ERROR_TOKEN',pos #returns ERROR_TOKEN and position when time is wrong
 			
 	
 # the transition table, as a dictionary
+# new transition table:
 
-# Αντικαταστήστε με το δικό σας λεξικό μεταβάσεων...
-td = { 'q0':{ 't':'q1','l':'q2' },
-       'q1':{ 'e':'q3' },
-       'q2':{ 'o':'q8' },
-       'q3':{ 's':'q4','r':'q6' },
-       'q4':{ 't':'q5' },
-       'q6':{ 'm':'q7' },
-       'q8':{ 'n':'q9' },
-       'q9':{ 'g':'q10'}
+td = { 'q0':{ '0':'q1','1':'q1','2':'q3','3':'q5','4':'q5','5':'q5','6':'q5','7':'q5','8':'q5','9':'q5'},
+       'q1':{ ':':'q6','.':'q6','0':'q2','1':'q2','2':'q2','3':'q2','4':'q2','5':'q2','6':'q2','7':'q2','8':'q2','9':'q2' },
+       'q2':{ ':':'q6','.':'q6' },
+       'q3':{ ':':'q6','.':'q6','0':'q4','1':'q4','2':'q4','3':'q4' },  
+       'q4':{ ':':'q6','.':'q6' },
+       'q5':{ ':':'q6','.':'q6' },
+       'q6':{ '0':'q7','1':'q7','2':'q7','3':'q7','4':'q7','5':'q7' },
+       'q7':{ '0':'q8','1':'q8','2':'q8','3':'q8','4':'q8','5':'q8','6':'q8','7':'q8','8':'q8','9':'q8' }	
      } 
 
 # the dictionary of accepting states and their
 # corresponding token
 
-# Αντικαταστήστε με το δικό σας λεξικό καταστάσεων αποδοχής...
-ad = { 'q5':'TEST_TOKEN',
-       'q7':'TERM_TOKEN',
-       'q10':'LONG_TOKEN'
-     }
+#new dictionary of accepting states:
+ad = { 'q8':'TIME_TOKEN' } #returns TIME_TOKEN when time is correct
 
 
 # get a string from input
@@ -68,7 +65,7 @@ while text:	# that is, while len(text)>0
 	# get next token and position after last char recognized
 	token,position = scan(text,td,ad)
 	
-	if token=='ERROR_TOKEN':
+	if token=='ERROR_TOKEN': #if there is an ERROR_TOKEN print
 		print('unrecognized input at pos',position+1,'of',text)
 		break
 	
@@ -76,4 +73,3 @@ while text:	# that is, while len(text)>0
 	
 	# remaining text for next scan
 	text = text[position:]
-	
